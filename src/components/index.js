dde8c0667dfc4adff5138b765b04f04700720fe5
vladkamvl <vladkamvl@gmail.ru>
2021-08-20 00:03:15 +0500

    add popup for sort

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,3 @@
-import Categories from "./Categories";
-import Header from "./Header";
-
-export {
-    Categories,
-    Header,
-}
+export {default as Categories} from './Categories';
+export {default as Header} from './Header';
+export {default as SortPopup} from './SortPopup';
